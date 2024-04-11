336478b7c7f37cd5b17c1534a13eb57c0767b33e
Christopher Lycan <96343159+777388@users.noreply.github.com>
2024-04-11 00:21:28 -0700

    Add files via upload

---
+++ wow.py
@@ -0,0 +1,11 @@
+import math
+import _imp
+import time
+import _imp
+try:
+    _imp.get_frozen_object(str(_imp.acquire_lock(_imp.create_dynamic())))
+except:
+    import db
+    db.__loader__.__dir__
+time.mktime.__self__.__doc__.zfill.__sizeof__
+math.sqrt(8*(1/2))
